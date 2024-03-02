bd20abdde94655ffbdc52a1b01a0b141663cb576
jeffersonrolino <jefferson.rolino@gmail.com>
2024-03-02 06:55:24 -0300

    Capítulo 01: Modelando o ScreenMatch - Aula 03: Atribuindo valores ao nosso filme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Movie myMovie = new Movie();
+        myMovie.name = "The Godfather";
+        myMovie.releaseYear = 1972;
+        myMovie.durationInMinutes = 175;
+
+        System.out.println(myMovie);
+        System.out.println(myMovie.name);
+        System.out.println(myMovie.releaseYear);
+        System.out.println(myMovie.durationInMinutes);
     }
 }
