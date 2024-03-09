d249c7daed2d815f8832edd64d22e7e780e6e8d2
jeffersonrolino <jefferson.rolino@gmail.com>
2024-03-09 09:23:08 -0300

    Curso 04 - Capítulo 04 - Aula 05: Escrevendo a lista de filmes em json

--- src/br/com/alura/screenmatch/main/MainWithSearch.java
+++ src/br/com/alura/screenmatch/main/MainWithSearch.java
@@ -14,57 +14,59 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainWithSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
+        String busca = "";
+        List<Title> titles = new ArrayList<>();
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).setPrettyPrinting().create();
 
-        System.out.println("Digite um filme para busca: ");
-        String busca = scanner.nextLine();
+        while(!busca.equalsIgnoreCase("sair")){
+            System.out.println("Digite o nome de filme para busca ou sair: ");
+            busca = scanner.nextLine();
 
-        String movie = busca.toLowerCase().replace(" ", "+");
+            if(busca.equalsIgnoreCase("sair")){
+                break;
+            }
 
-        String stringQuery;
-        String apiAddress = "https://www.omdbapi.com/?";
+            String movie = busca.toLowerCase().replace(" ", "+");
 
-        stringQuery = apiAddress + "t=" + movie + "&" + "apikey=" + Env.getApiKey();
+            String stringQuery;
+            String apiAddress = "https://www.omdbapi.com/?";
+            stringQuery = apiAddress + "t=" + movie + "&" + "apikey=" + Env.getApiKey();
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(stringQuery))
-                .build();
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(stringQuery))
+                    .build();
 
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
 
-        String json = response.body();
+            String json = response.body();
 
-        System.out.println(json);
+            TitleOmdb myTitleOmdb = gson.fromJson(json, TitleOmdb.class);
 
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-//        Title myTitle = gson.fromJson(json, Title.class);
+            try{
+                Title title = new Title(myTitleOmdb);
+                titles.add(title);
 
-        TitleOmdb myTitleOmdb = gson.fromJson(json, TitleOmdb.class);
-
-        System.out.println(myTitleOmdb);
-
-        try{
-            Title title = new Title(myTitleOmdb);
-            System.out.println("Título já convertido...");
-            System.out.println(title);
-
-            FileWriter fileWriter = new FileWriter("movies.txt");
-            fileWriter.write(title.toString());
-            fileWriter.close();
-
-        } catch (yearConversionException exception){
-            System.out.println("Aconteceu um erro: ");
-            System.out.println(exception.getMessage());
+            } catch (yearConversionException exception){
+                System.out.println("Aconteceu um erro: ");
+                System.out.println(exception.getMessage());
+            }
         }
+        System.out.println(titles);
+
+        FileWriter fileWriter = new FileWriter("titles.json");
+        fileWriter.write(gson.toJson(titles));
+        fileWriter.close();
 
         System.out.println("Programa finalizou corretamente...");
-
         scanner.close();
     }
 }
